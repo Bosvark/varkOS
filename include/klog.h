22daf3326b38fde6173005b99aa0d2a25c3a7773
Dieter Oelofse <dietero2@verifone.com>
2013-08-13 18:37:22 +0200

    Added klogBin

--- include/klog.h
+++ include/klog.h
@@ -2,5 +2,6 @@
 #define _KLOG_H_
 
 void klogInt(const char *description, unsigned int intval);
+void klogBin(const char *description, unsigned char *bindata, unsigned int binlen);
 
 #endif // _KLOG_H_
