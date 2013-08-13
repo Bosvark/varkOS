b9014f2a549617a46b58a323233d59015ba820e9
Dieter Oelofse <dietero2@verifone.com>
2013-08-13 18:37:47 +0200

    Added klogBin (untested)

--- klog.c
+++ klog.c
@@ -1,34 +1,63 @@
 #include "klog.h"
 #include "uart.h"
 #include "string.h"
+#include "timer.h"
 
 void hex_2_ascii(unsigned char *hex, char *ascii, unsigned int hex_len);
-
+void dbg(const char *str)
+{
+	while(*str){
+		uart_putc(*str++);
+		waitUS(100000);
+	}
+}
 void klogInt(const char *description, unsigned int intval)
 {
-    char buffer[9];
-    unsigned char intbuff[20];
-    (void)description;
+    char buffer[20];
+
     memset(buffer, 0, sizeof(buffer));
-    memset(intbuff, 0, sizeof(intbuff));
-    memcpy((unsigned char*)intbuff, &intval, sizeof(intval));
-    
-    hex_2_ascii(intbuff, buffer, sizeof(intbuff));
-    
-    uart_puts(description);
-    uart_puts("0x");
-    uart_puts(buffer);
-    uart_puts("\r\n");
+intval=0;
+//    hex_2_ascii((unsigned char*)&test, buffer, sizeof(intval));
+hex_2_ascii((unsigned char*)"\x31\x32\x33\x34", buffer, sizeof(intval));
+(void)description;
+/*
+    dbg(description);
+    dbg("0x");
+//    uart_puts(buffer);
+    dbg(buffer);
+*/    dbg("\r\n");
+
 }
 
-const char *ascii_def={"0123456789ABCDEF"};
+void klogBin(const char *description, unsigned char *bindata, unsigned int binlen)
+{
+    char buffer[512];
+
+    memset(buffer, 0, sizeof(buffer));
+
+    hex_2_ascii(bindata, buffer, binlen);
+
+    uart_puts(description);
+    uart_puts("[");
+    uart_puts(buffer);
+    uart_puts("]\r\n");
+}
+
+//
+// Local functions
+//
+
+static const char *ascii_def={"0123456789ABCDEF"};
 
 void hex_2_ascii(unsigned char *hex, char *ascii, unsigned int hex_len)
 {
-    uint32_t i,pos=0;
-    
-    for(i=0; i<hex_len; i++){
-        ascii[pos++] = ascii_def[(hex[i] >> 4)&0x0f];
-        ascii[pos++] = ascii_def[hex[i]&0x0f];
+	int pos=0;
+
+	while(pos < hex_len){
+uart_putc(hex[pos]);
+        *ascii++ = *(ascii_def + ((hex[pos] >> 4) & 0x0f));
+        *ascii++ = *(ascii_def + (hex[pos] & 0x0f));
+        pos++;
     }
 }
+
