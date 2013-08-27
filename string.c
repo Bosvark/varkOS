f1b94dc62c5744f644a089866c95ece19e233868
Dieter Oelofse <dietero2@verifone.com>
2013-08-27 18:24:40 +0200

    Added itoa

--- string.c
+++ string.c
@@ -23,3 +23,31 @@ void *memcpy(void *dest, const void *src, size_t count)
 		*tmp++ = *s++;
 	return dest;
 }
+
+char *itoa(int val)
+{
+	static char ascii_val[11];
+	int pos=0, temp=val;
+
+	// First round is to get the length
+	if(val<0)
+		pos++;
+
+	while(temp && (pos<=10)){
+		temp = temp/10;
+		pos++;
+	}
+
+	ascii_val[pos] = 0;
+
+	// Second round is to do the actual conversion
+	if(val<0)
+		ascii_val[--pos] = '-';
+
+	while(val && pos--){
+		ascii_val[pos] = 0x30 + (val%10);
+		val = val/10;
+	}
+
+	return ascii_val;
+}
