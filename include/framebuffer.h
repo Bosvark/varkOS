7008393123af2903ce28bc21393f5a938c1ace11
Dieter Oelofse <dietero2@verifone.com>
2013-08-31 15:28:39 +0200

    Removed FramebufferInfo structure

--- include/framebuffer.h
+++ include/framebuffer.h
@@ -1,20 +1,7 @@
 #ifndef _FRAMEBUFFER_H_
 #define _FRAMEBUFFER_H_
 
-typedef struct __attribute__ ((aligned (16))){
-    uint32_t physical_width;
-    uint32_t physical_height;
-    uint32_t virt_width;
-    uint32_t virt_height;
-    uint32_t pitch;
-    uint32_t depth;
-    uint32_t x;
-    uint32_t y;
-    uint32_t *gpu_ptr;
-    uint32_t size;
-}FramebufferInfo;
-
-uint32_t FramebufferInit(FramebufferInfo *fb_info);
+uint32_t FramebufferInit(void);
 void console_write(const char *msg);
 
 #endif // _FRAMEBUFFER_H_
